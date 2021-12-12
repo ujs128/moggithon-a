ab0f940a2d814998013c61c4066854216a6dd903
mei <meilate0211@gmail.com>
2021-12-12 14:54:22 +0900

    完成

---
+++ n-aquarium/script.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.move', {
+  duration: 900, // アニメーションの完了にかかる時間
+  viewFactor: 0.65, // 0~1,どれくらい見えたら実行するか
+  reset: false   // 何回もアニメーション表示するか
+});
+
+ScrollReveal().reveal('.toptext', {
+  duration: 3000, // アニメーションの完了にかかる時間
+  reset: false   // 何回もアニメーション表示するか
+});
